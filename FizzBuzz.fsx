36864801ffed812c61e8b66f59341f29f81742d5
Timothé Larivière <timothe.lariviere@outlook.com>
2017-08-08 07:40:15 +0200

    Second version of FizzBuzz

--- FizzBuzz.fsx
+++ FizzBuzz.fsx
@@ -6,3 +6,23 @@ let fizzBuzz n =
     | x -> x.ToString()
 
 [|1..100|] |> Array.map fizzBuzz |> Array.iter (fun s -> printfn "%s" s)
+
+// ----------------------
+
+let tellIf x str (y, s) =
+    let newS = if y % x = 0 then s+str else s
+    (y, newS)
+
+let tellNumberIfNothing (y, s) =
+    match s with
+    | "" -> string y
+    | x -> x
+
+let init x = (x, "")
+
+let fizz3 = tellIf 3 "Fizz"
+let buzz5 = tellIf 5 "Buzz"
+
+let fizzBuzz2 = init >> fizz3 >> buzz5 >> tellNumberIfNothing
+ 
+[|1..100|] |> Array.map fizzBuzz2 |> Array.iter (fun s -> printfn "%s" s)
